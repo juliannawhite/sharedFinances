951cedd758d24881d0142334650bc6b77cd4bd01
hchotwani <hchotwani@berkeley.edu>
2019-07-24 19:18:14 -0400

    difference between displays + working log

--- p4/settings.py
+++ p4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*6t50m_p^gif@4sz!590)u_!#9c-u8l9+7-v+*_op=v^#7wkv9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['p3-himanshuc71-hchotwani921366.codeanyapp.com']
+ALLOWED_HOSTS = ['sharedFinances-hchotwani921366.codeanyapp.com']
 
 
 # Application definition
