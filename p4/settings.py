ba925a787dc9926b1535326b7d91e807bee9bf74
juliannawhite13 <juliannawhite13@gmail.com>
2019-07-25 03:55:26 -0400

    log and budget changes dynamically now

--- p4/settings.py
+++ p4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*6t50m_p^gif@4sz!590)u_!#9c-u8l9+7-v+*_op=v^#7wkv9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sharedFinances-hchotwani921366.codeanyapp.com']
+ALLOWED_HOSTS = ['sharedFinances-juliannawhite13758275.codeanyapp.com']
 
 
 # Application definition
